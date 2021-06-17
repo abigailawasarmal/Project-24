4f276ca1ac6f3c77cef3ddaa748a76e67a0edb18
abigailawasarmal <77333713+abigailawasarmal@users.noreply.github.com>
2021-06-17 21:03:58 +0530

    Add files via upload

--- Rubber.js
+++ Rubber.js
@@ -1,13 +1,11 @@
 class Rubber{
 	constructor(x,y,r)
-
 	{
 		var options={
 			restitution:0.3,
 			friction:5,
 			density:1
 		}
-	// assign options to the rubber ball
 		this.x=x;
 		this.y=y;
 		this.r=r
@@ -21,15 +19,10 @@ class Rubber{
 			push()
 			translate(rubberpos.x, rubberpos.y);
 			rectMode(CENTER)
-
-			ellipseMode(RADIUS);
-			ellipse(0,0,this.r,this.r);
-			
 			strokeWeight(4);
 			stroke("black");
 			fill("darkblue");
-			//draw the ellipse here to display the rubber ball
-
+			ellipse(0,0,this.r, this.r);
 			pop()
 	}
 
--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var hammer;
+var stone, rubber,hammer;
 
 
 function setup(){
@@ -12,8 +12,14 @@ function setup(){
     world = engine.world;
 
     plane = new Plane(600,height,1200,20)
+    iron = new Iron(300,350);
+    stone = new Stone(700,320,100,100);
+  
+    rubber=new Rubber(900,450,70);
     hammer = new Hammer(10,100);
-    rubber = new Rubber(10,100);
+
+  
+
 
 }
 
@@ -21,11 +27,12 @@ function draw(){
     background("lightBlue");
     Engine.update(engine);
 
-
+    stone.display();
     plane.display();
-    hammer.display();
+   
     rubber.display();
+    iron.display()  
+    hammer.display();
 
-    
  
 }
